2acba1633a9aba061065b5eb3f9bf3a1522372ce
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2023-11-25 18:07:22 +0900

    task4 is done

--- task4_2.py
+++ task4_2.py
@@ -3,14 +3,14 @@ import numpy as np
 a = np.array([[0, 0]])
 h = 0.1
 
-def LdW0(W0):
-    return 6 * W0
+def LdW0(W0, W1):
+    return 6 * W0 + 6 * W1 - 12
 
-def LdW1(W1):
-    return 4 * W1 + 4
+def LdW1(W0, W1):
+    return 6 * W0 + 10 * W1 - 16
 
 def dLW(W0, W1):
-    return np.array([LdW0(W0), LdW1(W1)])
+    return np.array([LdW0(W0, W1), LdW1(W0, W1)])
 
 def an(n):
     return a[n - 1] - h * dLW(a[n - 1][0], a[n - 1][1])
