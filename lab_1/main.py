79129fb4d062ca21233b474b16b236bfc780a22d
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2024-02-13 03:23:32 +0900

    вроде все

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 import csv
 from sympy import symbols, Eq, solve
 
 x = []
 y = []
+year = []
 
 filename = open('lab_1/linreg.csv', 'r')
 file = csv.DictReader(filename)
@@ -14,6 +16,7 @@ with open('lab_1/linreg.csv', 'r') as source:
             data = row.replace('\n', '').split('\t')
             x.append(float(data[3].replace(',', '.')))
             y.append(float(data[2].replace(',', '.')))
+            year.append(int(data[1]))
 
 x_sqr = 0
 xy = 0
@@ -26,4 +29,19 @@ a, b = symbols('a,b')
 eq1 = Eq((x_sqr * a + sum(x) * b), xy)
 eq2 = Eq((sum(x) * a + 21 * b), sum(y))
 
-print(solve((eq1, eq2), (a, b)))
+params = solve((eq1, eq2), (a, b))
+
+y_plt = []
+
+for i in range(0, len(x)):
+    y_plt.append(params[a] * x[i] - params[b])
+
+print(year, y_plt)
+plt.plot(year, y_plt, 'ro')
+plt.plot(year, y_plt)
+plt.ticklabel_format(style='plain', axis='x', useOffset=False)
+plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+plt.xlabel('Year')
+plt.ylabel('y')
+plt.grid()
+plt.show()
