9ad00a489b669b60c913f4da6e6d7e90107c9791
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2024-02-17 02:12:39 +0900

    вот теперь все

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import csv
 from sympy import symbols, Eq, solve
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
+N = 2
 x = []
 y = []
-year = []
 
 filename = open('lab_1/linreg.csv', 'r')
 file = csv.DictReader(filename)
@@ -16,7 +18,6 @@ with open('lab_1/linreg.csv', 'r') as source:
             data = row.replace('\n', '').split('\t')
             x.append(float(data[3].replace(',', '.')))
             y.append(float(data[2].replace(',', '.')))
-            year.append(int(data[1]))
 
 x_sqr = 0
 xy = 0
@@ -31,17 +32,29 @@ eq2 = Eq((sum(x) * a + 21 * b), sum(y))
 
 params = solve((eq1, eq2), (a, b))
 
+print(params)
+
 y_plt = []
+x_plt = []
 
 for i in range(0, len(x)):
-    y_plt.append(params[a] * x[i] - params[b])
+    y_plt.append(y[i] + 2 * N)
+    x_plt.append(x[i] + 5 * N)
 
-print(year, y_plt)
-plt.plot(year, y_plt, 'ro')
-plt.plot(year, y_plt)
-plt.ticklabel_format(style='plain', axis='x', useOffset=False)
-plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
-plt.xlabel('Year')
-plt.ylabel('y')
-plt.grid()
+x_data = np.array(x_plt).reshape(-1, 1)
+y_data = np.array(y_plt)
+
+model = LinearRegression()
+model.fit(x_data, y_data)
+
+y_pred = model.predict(np.arange(120, 180).reshape(-1, 1))
+x2 = np.arange(120, 180)
+
+plt.plot(x_data, y_data, '.')
+plt.plot(x2, y_pred)
+#plt.ticklabel_format(style='plain', axis='x', useOffset=False)
+#plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+#plt.xlabel('Year')
+#plt.ylabel('y')
+#plt.grid()
 plt.show()
